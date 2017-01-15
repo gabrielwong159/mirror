cc00f2355daffc780956e5e6025de04f87df72e9
Tzeusy <uniquosity@gmail.com>
2017-01-15 23:22:30 +0800

    Initial Calendar Files
    Adds php file that parses the google calendar embedder to remove the
    inherent .css call and redirect it to our own custom_calendar.css file.
    (credit
    http://www.webdesignerdepot.com/2012/04/integrating-google-calendar-with-your-website/).
    
    Adds further code and theorized functionalities of KEY_1 to KEY_8 in
    calendar.js.
    
    Adds references to calPage, calDiv, calDirectory, etc. within relevant
    files.
    
    STILL NEED TO:
    1) Figure out jQuery.Ajax to run our .php file from within the .js file.
    2) Create a "box" (ie. cursor) that can be controlled using the UP,
    DOWN, LEFT, RIGHT buttons so users can navigate by DAY in the
    month-displayed calendar
    3) Map an "Enter" key that allows users to view more information on
    events on specified date from (2)

--- views/pages/calendar.js
+++ views/pages/calendar.js
@@ -32,10 +32,16 @@ function calInit() {
 }
 
 function loadCalendar() {
-  var htmlString = "<p class = 'header'> Welcome to SUTD. Which <br /> location would you like displayed? </p>";
-  var img = "<img src = '/img/school.png'>";
-  $display.innerHTML = htmlString;
-  display.innerHTML += img;
+  hideBox("info");
+  hideBox("main");
+  hideBox("motd");
+  showBox("caldiv");
+  var htmlString = "<?php <br />$your_google_calendar='https://calendar.google.com/calendar/embed?src=uniquosity%40gmail.com&ctz=Asia/Singapore'; <br />$url= parse_url($your_google_calendar);<br />$google_domain = $url['scheme'].'://'.$url['host'].dirname($url['path']).'/';<br />$dom = new DOMDocument;<br />$dom->loadHTMLfile($your_google_calendar);<br />$css = $dom->getElementByTagName('link')->item(0);<br />$css_href = $css->getAttributes('href');<br />$css->setAttributes('href', $google_domain . $css_href);<br />$scripts = $dom->getElementByTagName('script')->item(0);<br />foreach ($scripts as $script) {<br />$js_src = $script->getAttributes('src');<br />if ($js_src) $script->setAttributes('src', $google_domain . $js_src);<br />}<br />$element = $dom->createElement('link');<br />$element->setAttribute('type', 'text/css');<br />$element->setAttribute('rel', 'stylesheet');<br />$element->setAttribute('href', '/css/custom_calendar.css');<br />$head = $dom->getElementByTagName('head')->item(0);<br />$head->appendChild($element);<br />echo $dom->saveHTML();<br />?>"
+
+  //var htmlString = '<iframe src="https://calendar.google.com/calendar/embed?src=uniquosity%40gmail.com&ctz=Asia/Singapore" class = "calendar"></iframe> <link rel="stylesheet" href="/css/calendar.css">';
+  //var img = "<img src = '/img/school.png'>";
+  caldiv.innerHTML = htmlString;
+  //display.innerHTML += img;
 	calDirectory[KEY_1] = mainPage.init.bind(mainPage);
 	calDirectory[KEY_2] = leftMonth;
 	calDirectory[KEY_3] = rightMonth;
