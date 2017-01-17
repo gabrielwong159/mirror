fa0399aa77fc748c40880fe5f0a6770ce010315f
Tzeusy <uniquosity@gmail.com>
2017-01-17 18:40:48 +0800

    Calendar update
    Css works, haven't customized. HTML has to be edited using js for live
    mapping of months (what day is March 1st?) according to current
    Month/Year.
    
    Google Cal API call JSON code is live, but parsing and assigning yet to
    be done.
    
    TO DO:
    1) CSS+JS Cal
    2) JSON API Parsing
    3) Day Page HTML/IFRAME/json
    4) Button functionality (^v<> day, <> month, SELECT day to go to Day
    Page, (1) goes back to main).

--- views/pages/calendar.js
+++ views/pages/calendar.js
@@ -31,34 +31,84 @@ function calInit() {
 	loadCalendar();
 }
 
+
+var months = {
+  '1' : 'January',
+  '2' : 'February',
+  '3' : 'March',
+  '4' : 'April',
+  '5' : 'May',
+  '6' : 'June',
+  '7' : 'July',
+  '8' : 'August',
+  '9' : 'September',
+  '10': 'October',
+  '11': 'November',
+  '12': 'December',
+}
+
+
 function loadCalendar() {
   hideBox("info");
   hideBox("main");
   hideBox("motd");
   showBox("caldiv");
-  jQuery.Ajax({
-    type: "GET",
-    url: "/php/custom_calendar.php",
-    dataType: "html",
-    success: function(data) {
-      console.log('success', data);
-    }
-  });
+  //THIS STUFF PROCESSES MY EVENTS FROM MY GOOGLE CALENDAR, RETURNS IN JSON
+  /*var scriptString = new XMLHttpRequest();
+     scriptString.open('GET', 'https://www.googleapis.com/calendar/v3/calendars/sutd.app@gmail.com/events?key=AIzaSyBxIYzfHxIhawdppf8YeL_7PgIdY1g0evI');
+     scriptString.onload = function() {
+       console.log(scriptString.responseText);
+       var calData = jQuery.parseJSON(scriptString.responseText);
+       var calHtml=''
 
-  var htmlString = "calendar url link info"
+       for (var obj in calData.items) {
+         caldiv.innerHTML = calData.items[obj].description;
+         caldiv[INTEGERPARSEDATE(calData.items.[obj].DATE)].innerHTML;
+       }
+     };
+   scriptString.send();
+   */
+   //END OF JSON FEED
 
-  //var htmlString = '<iframe src="https://calendar.google.com/calendar/embed?src=uniquosity%40gmail.com&ctz=Asia/Singapore" class = "calendar"></iframe> <link rel="stylesheet" href="/css/calendar.css">';
+
+   //var dateJS = Date.parse('MONTH/DAY/YEAR');
+   //caldiv.innerHTML = ''
+
+   var dateJS = Date.parse('1/1/2017');
+   console.log("getDay returns:" + dateJS.getDay());
+   console.log("Today is " + Date() + " and it is a " + Date.today().getDayName() + ".");
+
+   // Finding where to highlight by default, ie. today's date. Also stores today's dd, mm, yyyy.
+   var today = new Date();
+   var dd = today.getDate();
+   var mm = today.getMonth()+1; //January is 0!
+   var yyyy = today.getFullYear();
+   console.log(dd + " " + mm + " " + yyyy);
+   //document.getElementById(dd).style.color = "blue";
+   document.getElementById('calendar1').contentWindow.document.getElementById("monthAndYear").innerHTML = months[mm] + " " + yyyy;
+
+  //var htmlString = '<iframe src="https://calendar.google.com/calendar/embed?src=uniquosity%40gmail.com&ctz=Asia/Singapore" class = "calendar"></iframe> <link rel="stylesheet" href="/css/custom_calendar.css">';
   //var img = "<img src = '/img/school.png'>";
-  caldiv.innerHTML = htmlString;
+  //caldiv.innerHTML = htmlString;
   //display.innerHTML += img;
 	calDirectory[KEY_1] = mainPage.init.bind(mainPage);
 	calDirectory[KEY_2] = leftMonth;
 	calDirectory[KEY_3] = rightMonth;
-  calDirectory[KEY_4] = leftDay;
-  calDirectory[KEY_5] = rightDay;
-  calDirectory[KEY_6] = upDay;
-  calDirectory[KEY_7] = downDay;
-  calDirectory[KEY_8] = select;
+  //calDirectory[KEY_4] = leftDay;
+  //calDirectory[KEY_5] = rightDay;
+  //calDirectory[KEY_6] = upDay;
+  //calDirectory[KEY_7] = downDay;
+  //calDirectory[KEY_8] = select;
 }
 
 //I HAVENT EDITED THIS PAGE YET. JUST USED DIRECTIONS.JS AS A TEMPLATE.
+
+//SAMPLE AJAX CODE FOR FUTURE REFERENCE
+/*  var scriptString = new XMLHttpRequest();
+    scriptString.open('GET', 'htmlString');
+    scriptString.onload = function() {
+      console.log(scriptString.responseText);
+      caldiv.innerHTML = scriptString.responseText;
+    };
+  scriptString.send();
+*/
